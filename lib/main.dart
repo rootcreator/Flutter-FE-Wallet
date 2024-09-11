352abcfbe2a2153ca272f6dda512b57ec5ee89f6
Marcelinus Apakhade <maarrxxy@gmail.com>
2024-09-11 17:18:16 +0100

    Update main.dart
    v.1.main.dart_update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'providers/wallet_provider.dart';
-import 'screens/dashboard.dart';
+import './providers/auth_provider.dart';
+import './providers/wallet_provider.dart';
+import './screens/login_screen.dart';
+import './screens/dashboard_screen.dart';
+import './screens/deposit_screen.dart';
+import './screens/withdraw_screen.dart';
+import './screens/transfer_screen.dart';
+import './screens/transactions_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,15 +16,39 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => WalletProvider(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => WalletProvider()),
+      ],
       child: MaterialApp(
         title: 'Wallet App',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: DashboardScreen(),
+        theme: ThemeData(primarySwatch: Colors.blue),
+        initialRoute: '/',
+        routes: {
+          Routes.initialRoute: (context) => Consumer<AuthProvider>(
+                builder: (context, authProvider, _) {
+                  return authProvider.isAuthenticated
+                      ? DashboardScreen()
+                      : LoginScreen();
+                },
+              ),
+          Routes.dashboardRoute: (context) => DashboardScreen(),
+          Routes.depositRoute: (context) => DepositScreen(),
+          Routes.withdrawRoute: (context) => WithdrawScreen(),
+          Routes.transferRoute: (context) => TransferScreen(),
+          Routes.transactionsRoute: (context) => TransactionsScreen(),
+        },
       ),
     );
   }
+}
+
+class Routes {
+  static const String initialRoute = '/';
+  static const String dashboardRoute = '/dashboard';
+  static const String depositRoute = '/deposit';
+  static const String withdrawRoute = '/withdraw';
+  static const String transferRoute = '/transfer';
+  static const String transactionsRoute = '/transactions';
 }
