ff4b8d5531eabf0b3b7879eb917366af8781aec9
Marcelinus Apakhade <maarrxxy@gmail.com>
2024-10-04 17:59:50 +0100

    Update wallet_service.dart

--- lib/services/wallet_service.dart
+++ lib/services/wallet_service.dart
@@ -8,7 +8,7 @@ class WalletService {
   }
 
   // Fetch account
-  static Future<double> fetchBalance() async {
+  static Future<void> fetchAccount() async {
     final response = await ApiService.getRequest('/account');
     return response['account'];
   }
